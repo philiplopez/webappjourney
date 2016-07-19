06e9120fb1a0f6f58c58ca723ffac3ca4622fadc
Philip Lopez <philip.lopez@philiplopez.com>
2016-07-19 21:27:17 +1000

    introduced react-router and very basic config in client.js

--- src/js/client.js
+++ src/js/client.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import Router from 'react-router/lib/Router'
+import Route from 'react-router/lib/Route'
+import browserHistory from 'react-router/lib/browserHistory'
+
 class AppContainer extends React.Component {
   render () {
     return (
       <p>
-        Hola!
+        Bonjour tout le monde!
       </p>
     )
   }
 }
-ReactDOM.render(<AppContainer />, document.getElementById('app-container'))
+
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={AppContainer} />
+  </Router>
+)
+
+ReactDOM.render(router, document.getElementById('app-container'))

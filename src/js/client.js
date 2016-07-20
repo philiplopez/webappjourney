424e6edde91a34cdbe8bce424b86a589fd9b3aa2
Philip Lopez <philip.lopez@philiplopez.com>
2016-07-20 19:22:54 +1000

    introduced react-router

--- src/js/client.js
+++ src/js/client.js
@@ -3,22 +3,54 @@ import ReactDOM from 'react-dom'
 
 import Router from 'react-router/lib/Router'
 import Route from 'react-router/lib/Route'
+import IndexRoute from 'react-router/lib/IndexRoute'
+import Link from 'react-router/lib/Link'
 import browserHistory from 'react-router/lib/browserHistory'
 
-class AppContainer extends React.Component {
-  render () {
+class AppLayout extends React.Component {
+  render() {
     return (
-      <p>
-        Bonjour tout le monde!
-      </p>
+      <div>
+        <header>
+          <h1>A Web Application Journey</h1>
+        </header>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link></li>
+            <li>
+              <Link to="/about">About</Link></li>
+          </ul>
+        </nav>
+        <main>
+          {this.props.children}
+        </main>
+        <footer>
+          (c) 2016
+        </footer>
+      </div>
     )
   }
 }
 
+class HomePage extends React.Component {
+  render() {
+    return <p>You are Home.</p>
+  }
+}
+
+class AboutPage extends React.Component {
+  render() {
+    return <p>What <em>is</em> this site about?</p>
+  }
+}
 
 const router = (
   <Router history={browserHistory}>
-    <Route path="/" component={AppContainer} />
+    <Route path="/" component={AppLayout}>
+      <IndexRoute component={HomePage} />
+      <Route path="about" component={AboutPage} />
+    </Route>
   </Router>
 )
 

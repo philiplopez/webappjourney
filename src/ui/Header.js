782588dd1b1612e8abb5b0c3085ac23bd3e726e0
Philip Lopez <philip.lopez@philiplopez.com>
2016-08-01 20:59:30 +1000

    CSS working client and server rendering

--- src/ui/Header.js
+++ src/ui/Header.js
@@ -3,7 +3,6 @@ import styles from './Header.css'
 
 export default class Header extends React.Component {
   render() {
-    console.log("styles", JSON.stringify(styles))
     return (
       <header>
         <h1 className={styles.title}>A Web Application Journey</h1>
